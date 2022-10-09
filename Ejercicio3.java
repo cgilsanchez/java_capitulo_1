52257ab9d3314ff7f977193e1b17cf33d97a8031
cgilsanchez <cgilsanchez.02@gmail.com>
2022-10-09 12:14:36 +0200

    Added Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+public class Ejercicio3 {
+    public static void main(String[]args){
+        System.out.print("Dog Perro\n");
+        System.out.print("Laptop Portatil\n");
+        System.out.print("Alarm Alarma\n");
+        System.out.print("Arm Brazo\n");
+        System.out.print("Assist Ayuda\n");
+        System.out.print("Art Arte\n");
+        System.out.print("Apple Manzana\n");
+        System.out.print("Answer Respuesta\n");
+        System.out.print("Yellow Amarillo\n");
+        System.out.print("Colour Color\n");
+    }
+}
