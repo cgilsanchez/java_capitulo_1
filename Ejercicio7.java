6034d12134af4130c077cd780d6939ef9625667c
cgilsanchez <cgilsanchez.02@gmail.com>
2022-10-09 16:11:30 +0200

    added Ejercicios5-8

---
+++ Ejercicio7.java
@@ -0,0 +1,11 @@
+public class Ejercicio7 {
+    public static void main(String[]args){
+        System.out.print("    *\n");
+        System.out.print("   * *\n");
+        System.out.print("  *   *\n");
+        System.out.print(" *     *\n");
+        System.out.print("*********\n");
+    }
+}
+
+
