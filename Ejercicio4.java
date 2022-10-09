78879de38885852803ceae1051e77cb4ae7748cf
cgilsanchez <cgilsanchez.02@gmail.com>
2022-10-09 12:57:34 +0200

    Added Ejercicio4.java

---
+++ Ejercicio4.java
@@ -0,0 +1,11 @@
+public class Ejercicio4 {
+public static void main(String[]args){
+    System.out.print("  Lunes         Martes         Miecoles       Jueves         Viernes\n");
+    System.out.print("Programacion  S.Informaticos  E.Desarrollo   Programacion     Fol\n");
+    System.out.print("Programacion  S.Informaticos  E.Desarrollo   Programacion     Fol\n");
+    System.out.print("L.D.Marcas   S.Informaticos   Programacion   L.D.Marcas       Fol\n");
+    System.out.print("L.D.Marcas    Programacion    Programacion   L.D.Marcas     S.Informaticos\n");
+    System.out.print("B.Datos       Programacion     B.Datos        B.Datos       S.Informaticos\n");
+    System.out.print("B.Datos       E.Desarrollo     B.Datos        B.Datos       S.Informaticos\n");
+}
+}
