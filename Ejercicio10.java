2c68a4f31d18c612871420e43438a47f666371b8
cgilsanchez <cgilsanchez.02@gmail.com>
2022-10-10 18:38:37 +0200

    Fin

---
+++ Ejercicio10.java
@@ -0,0 +1,10 @@
+public class Ejercicio10 {
+    public static void main(String[]args){
+        System.out.println("\033[0;31m ▲▲▲▲▲▲▲");
+        System.out.println("\033[0;32m  ▼▼▼▼▼");
+        System.out.println("\033[0;33m   ▼▼▼");
+        System.out.print("\033[0;34m    ▼");
+
+    }
+} 
+
